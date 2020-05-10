955864e7fd8aa14e9a973a61b6af739d0f6769be
calciumchan <calcium.ca.chan@gmail.com>
2020-05-09 18:13:51 -0700

    more basic file structure stuff

---
+++ cicada.h
@@ -0,0 +1,2 @@
+#include 'cicada/window.h'
+#include 'cicada/handle.h'
---
+++ cicada/meta.h
@@ -0,0 +1,3 @@
+//SDL has different include paths based on the target hardware, this file makes sure youre including the correct way
+//this should be included in any file that uses SDL includes, which will probably be pretty much all of them
+#include <SDL2/SDL.h>
