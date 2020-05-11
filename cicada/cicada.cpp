0e8d8e82b5c2e1c6eb5e6bd4a71e6ec34ca4efcd
calciumchan <calcium.ca.chan@gmail.com>
2020-05-11 14:21:57 -0700

    getting frame rate stuff ready

--- cicada/cicada.cpp
+++ cicada/cicada.cpp
@@ -2,18 +2,23 @@
 #include "handle.h"
 
 bool g_quit = false;
+int g_tickrate = 60;
+int g_framelimit = 60;
 SDL_Event g_event;
 
 void cicada_start(){
-
+	//randomize
+	srand(10);
+	//
 }
 
 void cicada_step(){
-	while(SDL_PollEvent( &g_event ) != 0 ){
-		if( g_event.type = SDL_QUIT ){
+	while( SDL_PollEvent( &g_event ) != 0 ){
+		if( g_event.type == SDL_QUIT ){
 			g_quit = true;
 		}
 	}
+	SDL_Delay(1000/g_tickrate);
 }
 
 void cicada_draw(){
--- main.cpp
+++ main.cpp
@@ -2,14 +2,13 @@
 #include "cicada.h"
 int main( int argc, char* args[] ){
 	cicada_start();
-	bool quit = false;
 	ce_window testwindow;
 	testwindow.start();
 	while(cicada_quit() == false){
 		cicada_step();
 		cicada_draw();
 		testwindow.draw();
-		SDL_Delay(100);
+		SDL_Delay(1000/30);
 	}
 	testwindow.end();
 }
